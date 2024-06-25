8e7e278efe3087296284ebf0e6c2884bb6106421
RohanSaxena2020 <71786640+RohanSaxena2020@users.noreply.github.com>
2024-06-24 22:08:41 -0400

    Update TodoList.jsx

--- reactjs-todolist project/src/components/TodoList.jsx
+++ reactjs-todolist project/src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
     let todos = [
@@ -11,7 +12,10 @@ export default function TodoList() {
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
                 return (
-                    <li key ={todoIndex}>{todo}</li>
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+
+                    </TodoCard>
                 )
             })}
         </ul>
